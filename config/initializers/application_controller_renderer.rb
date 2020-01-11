4bc5fb98635b6fc69717a474626a1098a9db9f9d
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-01-11 07:59:45 +0000

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '023b1c30749619d2de105fa6b90ce39cec2e8232db7b8f9366cee4a18906cbc85d6f1bd20e7c7047221a2f7ddb6eace608a7aa716dfddc927fbfff26f90a28f0'
